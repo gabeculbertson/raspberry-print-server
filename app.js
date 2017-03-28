0d6881efd9e820c15a497d69002b7b8e20da5d47
gabeculbertson <gabeculbertson@gmail.com>
2017-03-28 16:32:51 -0400

    print fix

--- app.js
+++ app.js
@@ -4,14 +4,21 @@ var http = require('http');
 var fs = require('fs');
 var ejs = require('ejs');
 var bodyParser = require('body-parser');
-var pdf = require('html-pdf');
 var needle = require('needle');
 var multer = require('multer');
 
 var exec = require('child_process').exec;
-var cmd = 'lpr sample.pdf';
+var cmd = 'lpr upload/upload.pdf';
 
 var options = { width: "58mm", height: "100mm" };
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './upload')
+    },
+    filename: function (req, file, cb) {
+        cb(null, 'upload.pdf')
+  }
+});
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
@@ -21,28 +28,16 @@ app.get('/', function(req, res){
 	res.sendFile(__dirname + '/public/index.html');
 });
 
-app.get('/print', function(req, res){
+app.post('/print', function(req, res){
 	var html = ejs.render(fs.readFileSync('sample.html', 'utf8'), { text: fs.readFileSync('in.txt', 'utf8') });
 	res.send(html);
 });
 
-app.post('/print_pdf', multer({
-    dest: 'upload/',
-    rename: function(fieldname, filename) {
-        return "upload.pdf";
-    },
-    onFileUploadStart: function(file) {
-        console.log(file.originalname + ' is starting ...')
-    },
-    onFileUploadComplete: function(file) {
-        console.log(file.fieldname + ' uploaded to  ' + file.path);
-        exec(cmd, function(error, stdout, stderr) {
-		 	console.log(error);
-		 	console.log(stdout);
-		 	console.log(stderr);
-		});
-    }
-}).single('pdf'), function(req, res){
+var upload = multer({ storage: storage });
+app.post('/print_pdf', upload.single('pdf'), function(req, res, next){
+	exec(cmd, function(err, stdout, stderr){
+		console.log(err, stdout, stderr);
+	});
 	res.send('done');
 });
 
